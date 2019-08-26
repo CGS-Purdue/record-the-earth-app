a0f17c34586e52fcdb8072f1b0d7ffcb79f8283f
Matt Harris <mjharris2407@gmail.com>
2019-08-26 13:31:13 -0400

    adds view container utility

--- App/Views/CenterView.js
+++ App/Views/CenterView.js
@@ -1,59 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { View } from 'react-native';
-
-const style = {
-  centered_outer: {
-    width: '100%',
-    height: '100%',
-    display: 'flex',
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  centered_inner: {
-    flexBasis: '100%',
-    flexGrow: 1,
-    flexShrink: 1,
-    width: '100%',
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  center_row: {
-    flex: 1,
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  center_col: {
-    flexBasis: '100%',
-    width: '100%',
-    height: '100%',
-    display: 'flex',
-    flexGrow: 1,
-    flexShrink: 0,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-};
+import { Styles } from '../Theme';
 
 function CenterRowView({ children }) {
-  return <View style={style.center_row}>{children}</View>;
+  return (<View style={Styles.center_row}>{children}</View>)
 }
 
 function CenterColView({ children }) {
-  return <View style={style.center_col}>{children}</View>;
+  return <View style={Styles.center_col}>{children}</View>;
 }
 
 function CenterView({ children }) {
   return (
-    <View style={style.centered_outer}>
-      <View style={style.centered_inner}>
+    <View style={Styles.centered_outer}>
+      <View style={Styles.centered_inner}>
         {children}
       </View>
     </View>
--- App/Views/RootView.js
+++ App/Views/RootView.js
@@ -1,44 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { View } from 'react-native';
-import { ThemeColors } from '../Theme/Colors';
+import { ThemeColors } from '../Theme';
+import { Styles } from '../Theme/Stylesheet';
 
-
-
-
-function RootView ({ children }, props) {
-  let style = {
-    width: '100%',
-    height: '100%',
-    display: 'flex',
-    padding: 0,
-    margin: 0,
-    flex: 1,
-  };
-
-  if (props.background) {
-    style.background = props.background;
-  } else {
-    style.color = ThemeColors.bgMain;
-    style.background = ThemeColors.colorMain;
+export default class RootView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.viewRef = React.createRef();
+    this.background = 'rbga(0,0,0,0)';
+    this.absolute = false;
   }
 
-  return (
-    <View  {...props} style={style}>
-        {children}
-    </View>
-  )
+  componentDidMount() {
+
+  }
+  componentWillUnmount(){
+
+  }
+
+  getContainerStyle () {
+    if (this.props.absolute) {
+      return Styles.rootContainer;
+    } else {
+      return Styles.absoluteRootContainer;
+    }
+  }
+
+  render() {
+  const baseStyle = this.getContainerStyle();
+  const userStyle = this.props.style;
+
+    return (
+      <View {...this.props}
+        ref={this.viewRef}
+        style={[userStyle,baseStyle]}>
+        {this.props.children}
+      </View>
+    )
+  }
 }
-
-RootView.defaultProps = {
-  children: null,
-  absolute: false,
-  background: null,
-};
-
-RootView.propTypes = {
-  children: PropTypes.node,
-};
-
 
 export { RootView }
