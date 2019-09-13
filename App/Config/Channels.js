74f7649413e47fb747f7e23afa1057b9ecb8196a
Matt Harris <m@ttharr.is>
2019-09-13 16:56:38 -0400

    project files

---
+++ App/Config/Channels.js
@@ -0,0 +1,2 @@
+
+// expo publish:set --publish-id d9bd6b80 --release-channel production
--- App/init.js
+++ App/init.js
@@ -3,20 +3,33 @@ import { activateKeepAwake } from 'expo-keep-awake';
 // import { YellowBox } from 'react-native';
 import { initalAppSetup } from './LifeCycle/InitialSetup';
 // import { Log } from './Utilities/Log';
+import { AppRegistry, Platform } from 'react-native';
 import { EXPO_CONSTANTS } from './Utilities/AppData';
-
 import App  from './App';
+
+AppRegistry.registerComponent('RecordTheEarthExpoDevelopment', () => App);
+
+if (Platform.OS === 'web') {
+    AppRegistry.runApplication('RecordTheEarthExpoDevelopment', {
+        rootTag: document.getElementById('root'),
+    });
+} else {
+  initalAppSetup();
+}
+
 
 // Log._info('Init');
 if (__DEV__) {
-//   YellowBox.ignoreWarnings([
-//     'Warning: ...',
-//     'Warning: Async Storage',
-//     'Warning: NetInfo',
-//   ]);
-// console.log(this.process.env);
 
-  if (this.process.env.npm_config_RUN_REACT_DEVTOOLS) {
+  activateKeepAwake();
+
+  //   YellowBox.ignoreWarnings([
+  //     'Warning: ...',
+  //     'Warning: Async Storage',
+  //     'Warning: NetInfo',
+  //   ]);
+  // console.log(this.process.env);
+  // if (process.env.npm_config_RUN_REACT_DEVTOOLS) {
     // if (!this.process.env.APP_REACT_DEVTOOLS_CONNECTED) {
     // console.log('loading react-devtools');
     // const devtools_config = {localhost: 'localhost', port: 8097};
@@ -24,14 +37,14 @@ if (__DEV__) {
     // const react_devtool = connectToDevTools({devtools_config});
     // console.log('react_devtool', react_devtool, this.__REACT_DEVTOOLS_GLOBAL_HOOK__);
     // this.process.env.APP_REACT_DEVTOOLS_CONNECTED = 'true';
-    } else if (this.process.env.RUN_REACT_DEVTOOLS) {
+    // } else if (process.env.RUN_REACT_DEVTOOLS) {
 
-    } else {
+    // } else {
     // console.log('__REACT_DEVTOOLS_PORT__', window.__REACT_DEVTOOLS_PORT__);
     // console.log('__REACT_DEVTOOLS_GLOBAL_HOOK__', window.__REACT_DEVTOOLS_GLOBAL_HOOK__);
     // window.__REDUX_DEVTOOLS_EXTENSION__.connect
     // console.log( this.__REACT_DEVTOOLS_PORT__);
-    }
+    // }
 
     // window.__REDUX_DEVTOOLS_EXTENSION__
     // window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
@@ -42,8 +55,7 @@ if (__DEV__) {
     //   data: this.process.env,
     // });
 
-  activateKeepAwake();
 
 }
-initalAppSetup();
+
 registerRootComponent(App);
