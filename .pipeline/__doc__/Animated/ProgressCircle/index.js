0195167102c35932eac61ad18cc98c0e92e8e9c2
Matt Harris <m@ttharr.is>
2019-10-04 12:03:14 -0400

    cleanup notes and tempfiles

--- .pipeline/__doc__/Animated/ProgressCircle/index.js
+++
@@ -1,5 +0,0 @@
-import { ProgressCircle } from './lib/ProgressCircle';
-import { PieChart } from './lib/ProgressCircle';
-import { AnimatedSpring } from './lib/AnimatedSpring';
-
-export { ProgressCircle, PieChart, AnimatedSpring };
